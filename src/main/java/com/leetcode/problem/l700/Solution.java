5d95829ccc6a60a886204100868dcbea4dacb475
hugh <wangyx1122@gmail.com>
2020-08-27 22:26:48 +0800

    l700

---
+++ src/main/java/com/leetcode/problem/l700/Solution.java
@@ -0,0 +1,66 @@
+package com.leetcode.problem.l700;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * <pre>
+ *     给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+ *
+ * 例如，
+ *
+ * 给定二叉搜索树:
+ *
+ *         4
+ *        / \
+ *       2   7
+ *      / \
+ *     1   3
+ *
+ * 和值: 2
+ * 你应该返回如下子树:
+ *
+ *       2
+ *      / \
+ *     1   3
+ * 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-in-a-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * </pre>
+ *
+ * @author wangyongxu
+ * @date 2020/8/26 11:37 下午
+ */
+public class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.val == val) {
+                return node;
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return null;
+    }
+
+
+}
