43ff9a44576496b53105cbbc68d261721a92447b
wangyongxu <wangyongxu@hikvision.com.cn>
2020-08-27 15:15:51 +0800

    feat(leetcode): 100

--- src/main/java/com/leetcode/problem/l100/Solution.java
+++ src/main/java/com/leetcode/problem/l100/Solution.java
@@ -90,4 +90,22 @@ public class Solution {
         }
         return true;
     }
+
+    public boolean isSameTree1(TreeNode p, TreeNode q) {
+        if ((p != null && q != null) && (p.val != q.val)) {
+            return false;
+        }
+        if ((p == null) && (q != null)) {
+            return false;
+        }
+        if ((p != null) && (q == null)) {
+            return false;
+        }
+        if (p == null && q == null) {
+            return true;
+        }
+
+        return isSameTree1(p.left, q.left) || isSameTree1(p.right, q.right);
+
+    }
 }
