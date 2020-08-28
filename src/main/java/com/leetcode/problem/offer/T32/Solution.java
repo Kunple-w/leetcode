c4e168e4435f461e8e256b8a6108def964788e3b
wangyongxu <wangyongxu@hikvision.com.cn>
2020-08-28 10:17:17 +0800

    feat(leetcode): offer 32

---
+++ src/main/java/com/leetcode/problem/offer/T32/Solution.java
@@ -0,0 +1,81 @@
+package com.leetcode.problem.offer.T32;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * <pre>
+ *     剑指 Offer 32 - III. 从上到下打印二叉树 III
+ * 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+ *
+ *
+ *
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其层次遍历结果：
+ *
+ * [
+ *   [3],
+ *   [20,9],
+ *   [15,7]
+ * ]
+ *
+ *
+ * 提示：
+ *
+ * 节点总数 <= 1000
+ * </pre>
+ *
+ * @author wangyongxu
+ * @date 2020/8/28 10:03
+ */
+public class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> r = new ArrayList<>();
+        if (root == null) {
+            return r;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean flag = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> tmp = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (flag) {
+                    tmp.add(node.val);
+                } else {
+                    tmp.add(0, node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            flag = !flag;
+            r.add(tmp);
+        }
+        return r;
+    }
+}
